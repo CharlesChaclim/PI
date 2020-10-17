be49db737817b29848225c6eb2e6b01c65f94b39
CharlesChaclim <33669828+CharlesChaclim@users.noreply.github.com>
2020-10-17 12:35:27 -0300

    2

--- box_filter/PI.py
+++ box_filter/PI.py
@@ -5,7 +5,7 @@ import math
 
 parser = ArgumentParser()
 parser.add_argument("-P", "--path", dest="path", default='../Imagens/Grayscale/67079.jpg', help="caminho da imagem")
-parser.add_argument("-S", "--scale", dest="scale", default='2', help="valor da escala aplicada")
+parser.add_argument("-S", "--scale", dest="scale", default='5', help="valor da escala aplicada")
     
 args = parser.parse_args()
 
@@ -16,11 +16,11 @@ cv2.imshow("Imagem Original", image)
 scale = int(args.scale)
 
 lin = []
-for i in range(0,image.shape[0],scale):
-	for j in range(0,image.shape[1],scale):
+for i in range(0,image.shape[0]):
+	for j in range(0,image.shape[1]):
 		lin.append(int(image[i:scale+i,j:scale+j].mean()))
 
-FImage = np.repeat(np.repeat(np.array(np.reshape(lin,(math.ceil(image.shape[0]/scale),math.ceil(image.shape[1]/scale))), dtype=np.uint8), scale, axis=1), scale, axis=0)
+FImage = np.array(np.reshape(lin,(image.shape[0],image.shape[1])), dtype=np.uint8)
 
 cv2.imshow('Box filter', FImage)
 cv2.waitKey(0)
