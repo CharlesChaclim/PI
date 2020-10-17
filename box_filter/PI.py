b2493a9e1de8161bf2d8a364d0fe5603ab9f3564
CharlesChaclim <33669828+CharlesChaclim@users.noreply.github.com>
2020-10-17 09:40:14 -0300

    "Avanço"

--- box_filter/PI.py
+++ box_filter/PI.py
@@ -4,18 +4,26 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("-P", "--path", dest="path", default='../Imagens/Grayscale/67079.jpg', help="caminho da imagem")
-parser.add_argument("-S", "--scale", dest="scale", default='4', help="valor da escala aplicada")
+parser.add_argument("-S", "--scale", dest="scale", default='2', help="valor da escala aplicada")
     
 args = parser.parse_args()
 
 path = args.path
 image = cv2.imread(path, 1)
-cv2.imshow("Imagem Original", image)
+#cv2.imshow("Imagem Original", image)
 
 scale = int(args.scale)
 
-FImage = np.zeros((int(image.shape[1]/scale),int(image.shape[1]/scale)), np.uint64)
-print(FImage)
+col = []
+lin = []
+for i in range(0,image.shape[0],scale):
+	for j in range(0,image.shape[1],scale):
+		col.append(int(image[i:scale+i,j:scale+j].mean()))
+	lin.append(col)
+	col = []
 
+FImage = np.repeat(np.repeat(np.array(lin, dtype=np.uint8), scale, axis=1), scale, axis=0)
+
+cv2.imshow('Box filter', FImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
