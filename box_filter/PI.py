d99ae8b57ff6e3d500d5b87d4db7408980174b84
CharlesChaclim <33669828+CharlesChaclim@users.noreply.github.com>
2020-10-17 11:15:06 -0300

    Add files via upload

--- box_filter/PI.py
+++ box_filter/PI.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from argparse import ArgumentParser
+import math
 
 parser = ArgumentParser()
 parser.add_argument("-P", "--path", dest="path", default='../Imagens/Grayscale/67079.jpg', help="caminho da imagem")
@@ -10,19 +11,16 @@ args = parser.parse_args()
 
 path = args.path
 image = cv2.imread(path, 1)
-#cv2.imshow("Imagem Original", image)
+cv2.imshow("Imagem Original", image)
 
 scale = int(args.scale)
 
-col = []
 lin = []
 for i in range(0,image.shape[0],scale):
 	for j in range(0,image.shape[1],scale):
-		col.append(int(image[i:scale+i,j:scale+j].mean()))
-	lin.append(col)
-	col = []
+		lin.append(int(image[i:scale+i,j:scale+j].mean()))
 
-FImage = np.repeat(np.repeat(np.array(lin, dtype=np.uint8), scale, axis=1), scale, axis=0)
+FImage = np.repeat(np.repeat(np.array(np.reshape(lin,(math.ceil(image.shape[0]/scale),math.ceil(image.shape[1]/scale))), dtype=np.uint8), scale, axis=1), scale, axis=0)
 
 cv2.imshow('Box filter', FImage)
 cv2.waitKey(0)
